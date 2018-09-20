7aba7335191ea1b432604bb1e0b609a5f95f83e3
Marcin Mentel <marcin.mentel@gmail.com>
2018-09-20 21:18:18 +0200

    new lesson

---
+++ S_3_inputuser/app.js
@@ -0,0 +1,13 @@
+console.log('Starting app.js in usr');
+
+
+const fs = require('fs'); // ładowanie modułu , fs moduł służący do ładaowania plików
+const _ = require('lodash');
+
+const notes = require('./notes.js')// dołączenie i uruchomienie pliku note.js
+
+
+
+
+
+
