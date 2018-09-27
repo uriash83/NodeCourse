8ffb653d937a6f0ff67b83b029451236ce98e1b2
Marcin Mentel <marcin.mentel@gmail.com>
2018-09-27 10:48:58 +0200

    webserver

--- S_5_webserver/server.js
+++ S_5_webserver/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -11,6 +13,25 @@ hbs.registerHelper('screamIt',(text)=>{
 });
 app.set('view engine','hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next)=>{
+    var now =new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log('Unable co write to file');
+        }
+    });
+    next();
+});
+// użycie tego poniżej , przekierwuje każdą stroną na maintenance.hbs. 
+// ale niedotyczy to np. help.html której ścieżka jest statyczna 
+// żeby to zablokować należy: app.use(express.static(__dirname + '/public')); 
+// umieścić za poniższym middlware 
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+});
 
 app.get('/',(req,res)=>{
     //res.send('<h1>Hello Expres</h1>');
