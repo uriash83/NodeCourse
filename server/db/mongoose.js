87c05a7fd4b252b968b40dca220ca621b94365c5
Marcin Mentel <marcin.mentel@gmail.com>
2018-10-12 17:25:52 +0200

    d

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // dla local : mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
-mongoose.connect('mongodb://<uriash>:<1qazZXCVB>@ds249092.mlab.com:49092/climb',{ useNewUrlParser: true });
+mongoose.connect('mongodb://<uriash>:<1qazZXCVB>@ds249092.mlab.com:49092/climb');
 
 module.exports = {
     //mongoose: mongoose
